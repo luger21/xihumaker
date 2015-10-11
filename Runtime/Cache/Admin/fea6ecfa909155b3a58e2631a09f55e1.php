76ed370d42aa29049c8edb9bba828da1ae79cf15
luiz <zluger21@gmail.com>
2015-10-11 16:16:03 +0800

    第一版后台

---
+++ Runtime/Cache/Admin/fea6ecfa909155b3a58e2631a09f55e1.php
@@ -0,0 +1,39 @@
+<?php if (!defined('THINK_PATH')) exit();?><div class="span<?php echo ($addons_config["width"]); ?>">
+	<div class="columns-mod">
+		<div class="hd cf">
+			<h5>开发团队</h5>
+			<div class="title-opt">
+			</div>
+		</div>
+		<div class="bd">
+			<div class="sys-info">
+				<table>
+					<tr>
+						<th>项目开发</th>
+						<td>庄路</td>
+					</tr>
+					<tr>
+						<th>网站设计</th>
+						<td>俞杰</td>
+					</tr>
+					<tr>
+						<th>前端支持</th>
+						<td>万仁刚</td>
+					</tr>
+					<tr>
+						<th>网站首页</th>
+						<td><a href="http://www.xihumaker.com" target="_blank">www.xihumaker.com</a></td>
+					</tr>
+					<tr>
+						<th>官方QQ群</th>
+						<td><a target="_blank" href="http://wp.qq.com/wpa/qunwpa?idkey=14ba57a5273cc820d298cc394227276b22177217e413dfe658aa3d3b34c119e0"><img border="0" src="http://pub.idqqimg.com/wpa/images/group.png" alt="西湖创客交流群" title="西湖创客交流群"></a></td>
+					</tr>
+					<tr>
+						<th>BUG反馈</th>
+						<td>luger21@qq.com</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
