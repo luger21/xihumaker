1fe45f38c39bb842631cb45d408c9fd6bed2da74
luiz <zluger21@gmail.com>
2015-10-26 21:35:20 +0800

    js修改

--- Public/Home/js/main.js
+++ Public/Home/js/main.js
@@ -4,34 +4,29 @@ var J_curPage = J_activeList.data('curpage'); //当面页数
 var J_hasPage = J_activeList.data('haspage'); //判断是否存在分页
 //请求数据
 function autoData() {
-
+	J_curPage=J_curPage+1
 	$.ajax({
-			url: '/Activity/api/p/2',
+			url: '/Activity/api/p/'+J_curPage,
 			type: 'get',
 			dataType: 'json'
 		})
 		.done(function(data) {
-			console.log(data);
 			var J_html = [];
-
 			for (var i in data.list) {
-				
-				J_html.push("<li><div class='act-listl'><div class='act-icon act-icon4 fl'></div><div class='act-tit'><em>");
-				J_html.push("2019年<br>12月6日"); //时间
+				J_html.push("<li><div class='act-listl'><div class='act-icon "+data.list[i].act_icon+" fl'></div><div class='act-tit'><em>");
+				J_html.push(data.list[i].year+"<br>"+data.list[i].month); //时间
 				J_html.push("</em></div></div><div class='act-listr fr'>");
-				J_html.push("<img src='images/img1.jpg'>"); //标题
+				J_html.push("<img src="+data.list[i].cover_url+" />"); //标题
 				J_html.push("<div class='act-listrtext'><h5>");
 				J_html.push(data.list[i].title); //标题
 				J_html.push("</h5><p class='act-listCon'>");
 				J_html.push(data.list[i].description); //简介
-				J_html.push("</p><a href='detial.php'>阅读全文</a>");
+				J_html.push("</p><a href=Activity/detail?id="+data.list[i].id+">阅读全文</a>");
 				J_html.push("</div></div></li>");
 			}
-
 			if (!data.haspage) { //判断是否有下一页
 				J_hasPage = false;
 			};
-console.log(J_html.join(''));
 			$('#activeList').append(J_html.join(''))
 		})
 		.fail(function() {
@@ -41,7 +36,6 @@ console.log(J_html.join(''));
 			if (data.hasPage) {
 				J_hasPage = true;
 			}
-			console.log("complete");
 		});
 };
 $(window).scroll(function() {
@@ -52,14 +46,11 @@ $(window).scroll(function() {
 	console.log("页面的文档高度 ："+$(document).height());
 	console.log('浏览器的高度：'+$(window).height());*/
 	if (J_docHeight < (J_scrollTop + J_winHeight + 340)) {
-
 		//判断是否存在分页
 		if (J_hasPage) {
-			console.log('ddd');
 			J_hasPage = !J_hasPage; //等待请求完再进入下一次请求
 			setTimeout(autoData, 500)
 		}
-
 	}
 
 });
